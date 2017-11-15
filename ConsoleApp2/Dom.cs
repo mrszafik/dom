ca2cc244eca629cc6e18b5a6ce54c2841c641838
Maciej Szafrański <szafmac@gmail.com>
2017-11-15 16:21:24 +0100

    final

---
+++ ConsoleApp2/Dom.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class dom
+    {
+        public string adres;
+        public int metraz;
+        public string domownicy;
+        public int liczbaOkien;
+
+        public dom(string adres, int metraz, string domownicy, int liczbaOkien)
+        {
+            this.adres = adres;
+            this.metraz = metraz;
+            this.liczbaOkien = liczbaOkien;
+            this.domownicy = domownicy;
+        }
+
+        public int countTaxes()
+        {
+            return 10 * metraz;
+        }
+    }
+}
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,7 +6,15 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            dom dom1 = new dom("kwiatowa", 35, "Jadwiga", 5);
+            dom dom2 = new dom("takowa", 140, "Jan, Malgorzata, Jacek", 20);
+
+            Console.WriteLine("Ulica: " + dom1.adres + " Metraz: " + dom1.metraz + " Domownicy: " + dom1.domownicy
+                + " Ilosc okien: " + dom1.liczbaOkien + " Podatek: " + dom1.countTaxes());
+            Console.WriteLine("Ulica: " + dom2.adres + " Metraz: " + dom2.metraz + " Domownicy: " + dom2.domownicy
+                + " Ilosc okien: " + dom2.liczbaOkien + " Podatek: " + dom2.countTaxes());
+
+            Console.ReadKey();
         }
     }
 }
